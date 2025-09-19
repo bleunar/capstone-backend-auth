5258494d23e552eedd187f0a65131667d90a6612
tonyo <arabit.tr@gmail.com>
2025-09-20 06:25:51 +0800

    updated access_level fetch: file read method

--- app/services/access.py
+++ app/services/access.py
@@ -1,13 +1,16 @@
-import json
+import json, os
 
-def get_access_levels() -> list:
-    with open('access_levels.json', 'r') as f:
-        data_as_dict = json.load(f)
-    return data_as_dict
+def get_access_levels():
+    base = os.path.dirname(__file__)
+    path = os.path.join(base, "access_levels.json")
+    with open(path, "r") as f:
+        return json.load(f)
 
 
 def access_level_lookup() -> dict:
-    with open('access_levels.json', 'r') as f:
+    base = os.path.dirname(__file__)
+    path = os.path.join(base, "access_levels.json")
+    with open(path, "r") as f:
         data_dict = json.load(f)
 
     access_level_dict = {}
